d7f992042ad7b690ea411299abc8a39b15230144
rakesh <nrakesh84@gmail.com>
2014-07-13 00:39:33 +0100

    ok

--- GUI/src/test/Test.java
+++ GUI/src/test/Test.java
@@ -6,7 +6,8 @@ public class Test {
 		System.out.println("Test");
 		System.out.println("Test 1");
 		System.out.println("Test 2");
-		System.out.println("Test 3");		
+		System.out.println("Test 3");
+		System.out.println("Test 4");
 	}
 
 }
